1f9ec36cf6af3869233e9006e3eb7cd079e71f00
wayne0926 <wayne0926@foxmail.com>
2020-10-30 22:52:08 +0800

    更新

--- js.js
+++ js.js
@@ -1,9 +1,11 @@
-let u1 = 'cd2239dfc5da7bf418f43205229f2bd0'    // AOLIGEIGAN
-let p1 = '013c51c369d7e3bb7442e5c3ef67c29f'    // JJFBT0000
+let u1 = 'cd2239dfc5da7bf418f43205229f2bd0'// AOLIGEIGAN
+let p1 = '013c51c369d7e3bb7442e5c3ef67c29f'// JJFBT0000
 let u2 = 'acef29994e92b4c9e92e98b613651b99'
 let p2 = '90195ef78b13fa12f3310c3f48354b56'
-var us = [u1, u2]
-var ps = [p1, p2]
+let u3 = 'fe01ce2a7fbac8fafaed7c982a04e229'// demo
+let p3 = 'fe01ce2a7fbac8fafaed7c982a04e229'// demo
+var us = [u1, u2, u3]
+var ps = [p1, p2, u3]
 function myFunction() {
     var uu = md5(document.getElementById("username").value);
     var pp = md5(document.getElementById("password").value);
