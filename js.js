7326d6692349b88ece7b897c9cefd5a1eaefcd0f
wayne0926 <wayne0926@foxmail.com>
2020-10-31 13:56:04 +0800

    更新

--- js.js
+++ js.js
@@ -7,17 +7,52 @@ let p3 = 'fe01ce2a7fbac8fafaed7c982a04e229'// demo
 var us = [u1, u2, u3]
 var ps = [p1, p2, u3]
 function myFunction() {
-    var uu = md5(document.getElementById("username").value);
-    var pp = md5(document.getElementById("password").value);
-    var su = us.indexOf(uu)
+    var uu = document.getElementById("username").value;
+    var pp = document.getElementById("password").value;
+    var su = us.indexOf(md5(uu))
     if (su != '-1') {
         var sp = ps[su]
     }
-    if (su != '-1', sp == pp) {
-        alert("登录成功");
-        window.location.href = "%6C%69%2E%68%74%6D%6C";
+    if (su != '-1', sp == md5(pp)) {
+        Cookies.set('username', BASE64.encode(uu), { expires: 3 })
+        Cookies.set('password', BASE64.encode(pp), { expires: 3 })
+        // console.log(Cookies.get())
+        // alert("登录成功");
+        // window.location.href = "/user";
     }
     else {
+        if (Cookies.get('username') != null) {
+            Cookies.remove('username')
+            if (Cookies.get('password') != null) {
+                Cookies.remove('password')
+            }
+        }
         alert("用户名或密码错误");
     }
+}
+function login() {
+    if (Cookies.get('username') != null) {
+        var gu = BASE64.decode(Cookies.get('username'))
+        if (Cookies.get('password') != null) {
+            let gp = BASE64.decode(Cookies.get('password'))
+            let gur = us.indexOf(md5(gu))
+            if (gur != '-1') {
+                var gpr = ps[gur]
+            }
+            if (gur != '-1', gpr == md5(gp)) {
+                console.log('登录成功')
+                window.location.href = "/user"
+            }
+            else {
+                if (Cookies.get('username') != null) {
+                    Cookies.remove('username')
+                    if (Cookies.get('password') != null) {
+                        Cookies.remove('password')
+                    }
+                }
+            }
+        }
+    }
+
+    setTimeout(login, 3000);
 }
